51fdf0be846fba8645f84649ed97de0653dbaf0d
Alex Connolly <adac@berkeley.edu>
2025-02-27 09:51:08 -0700

    SAM_helpers outputs better reflect Fortran CLUBB. Mixing length can now be computed without size errors thrown.

--- subgrid_parameterization/preprocess/SAM_helpers.py
+++ subgrid_parameterization/preprocess/SAM_helpers.py
@@ -1,23 +1,43 @@
 import numpy as np
 from scipy.interpolate import interp1d
 
+def get_grid(ds,time_for_space=True):
+
+    nzm, nzt, zmv, ztv, dzmv, dztv, invrs_dzmv, invrs_dztv = stagger_grid(ds)
+
+    if time_for_space:    
+        ngrdcol = len(ds['time'])
+    else:
+        ngrdcol = None
+        return nzm, nzt, ngrdcol, zmv, ztv, dzmv, dztv, invrs_dzmv, invrs_dztv
+
+    zm = expand(zmv, ngrdcol)
+    zt = expand(ztv, ngrdcol)
+    dzm = expand(dzmv, ngrdcol)
+    dzt = expand(dztv, ngrdcol)
+    
+    return nzm, nzt, ngrdcol, zm, zt, dzm, dzt, 1.0/dzm, 1.0/dzt 
+
 def stagger_grid(z):
     try:
-        z = z['z'].values #in case an xarray Dataset is passed
+        z = z['z'].values #in case xarray Dataset is passed
     except:
         z = z
     
     nzm = (len(z) + 1)//2 #Adding surface then leapfrogging the averages to stagger
     nzt = nzm - 1
     
-    zm=np.concatenate(([0],0.5*(z[1::2]+z[2::2])))
+    zm=np.concatenate(([0],0.5*(z[1:2*nzm-1:2]+z[2:2*nzm-1:2])))
     zt=(zm[:-1]+zm[1:])/2
 
-    dzm = np.concatenate(([np.nan],np.diff(zt)))
+
+    dzm = np.concatenate( ( [2.0*(zt[0]-zm[0])], np.diff(zt), [np.diff(zt)[-1]] ) )
+                # gr%dzm(i,1) = 2.0_core_rknd * ( gr%zt(i,1) - gr%zm(i,1) )  
+                # gr%dzm(i,nzm) = gr%dzm(i,nzm-1) from subroutine setup_grid_heights in grid_class.F90
     dzt = np.diff(zm)
+
     invrs_dzm = 1.0/dzm
     invrs_dzt = 1.0/dzt
-
     
     return nzm, nzt, zm, zt, dzm, dzt, invrs_dzm, invrs_dzt
 
@@ -29,11 +49,25 @@ def stagger_var(var_str, ds, z='zt'):
             _, _, _, z, _, _, _, _ = stagger_grid(ds)
     
     if var_str=='p':
-        zaxis=0
+        vals=expand(ds[var_str].squeeze().values, ds)
     else:
-        zaxis=1
+        vals=ds[var_str].squeeze().values
+
+    return interp1d(ds['z'].values,vals,axis=1,fill_value='extrapolate')(z)
+
+def expand(u, ngrdcol):
+    try:
+        assert isinstance(ngrdcol, int)
+        return np.array([u for i in range(ngrdcol)])
+    except:
+        ngrdcol = len(ngrdcol['time']) #in case xarray Dataset is passed
+        return np.array([u for i in range(ngrdcol)])
+
     
-    return interp1d(ds['z'].values,ds[var_str].squeeze().values,axis=zaxis,fill_value='extrapolate')(z)
+    
+
+
+    
     
 
     
--- subgrid_parameterization/preprocess/mixing_length.py
+++ subgrid_parameterization/preprocess/mixing_length.py
@@ -3,135 +3,273 @@ import numpy as np
 def compute_mixing_length(nzm, nzt, ngrdcol, zm, zt, dzm, dzt, invrs_dzm, invrs_dzt, 
                           thvm, thlm, rtm, em, 
                           Lscale_max, p_in_Pa, exner, thv_ds, mu, lmin, 
-                          saturation_formula, l_implemented):
+                          saturation_formula, l_implemented=True):
     """
     Compute the mixing length using Larson's 5th moist, nonlocal length scale method.
     """
     
-       
     # Constants
     zlmin = 0.1
     Lscale_sfclyr_depth = 500.0
     grav = 9.81  # gravitational acceleration
-    Cp = 1004.0  # Specific heat capacity of air at constant pressure
+    cp = 1004.0  # Specific heat capacity of air at constant pressure
     Rd = 287.0  # Gas constant for dry air
     Lv = 2.5e6   # Latent heat of vaporization
     eps = 1e-6   # Small threshold value
-    ep = Rd / (Lv / Cp)
+    ep = Rd / (Lv / cp)
     ep1 = (1 - ep) / ep
     ep2 = 1 / ep
-
+    
+    # Compute turbulent kinetic energy (TKE)
+    tke_i = (em[:, :-1] + em[:, 1:]) / 2
+    
     # Initialize output arrays
     Lscale = np.full((ngrdcol, nzt), zlmin)
     Lscale_up = np.full((ngrdcol, nzt), zlmin)
     Lscale_down = np.full((ngrdcol, nzt), zlmin)
 
-    # Initialize temporary arrays
     grav_on_thvm = grav / thvm
-    Lv_coef = Lv / (exner * Cp) - ep2 * thv_ds
-
+    Lv_coef = Lv / (exner * cp) - ep2 * thv_ds
+    
     exp_mu_dzm = np.exp(-mu[:, None] * dzm)
-    print(mu.shape)
-    print(mu[:, None].shape)
-    print(invrs_dzm[:, None].shape)
-    print(thlm.shape)
     invrs_dzm_on_mu = invrs_dzm / mu[:, None]
-    print(invrs_dzm_on_mu[:, None].shape)
     entrain_coef = (1 - exp_mu_dzm) * invrs_dzm_on_mu
 
-    # Compute turbulent kinetic energy (TKE)
-    tke_i = zm2zt(em, ngrdcol)
+    # Compute auxiliary values
+    Lv2_coef = ep * Lv**2 / (Rd * cp)
+    invrs_Lscale_sfclyr_depth = 1.0 / Lscale_sfclyr_depth
 
-    # Upward Mixing Length Calculation
-    thl_par_j_precalc = thlm[:, 1:] - thlm[:, :-1] * exp_mu_dzm[:, 1:] - \
-                        (thlm[:, 1:] - thlm[:, :-1]) * entrain_coef[:, 1:]
+    ## Upward Mixing Length Calculation
+    
+    thl_par_j_precalc = np.full(thlm.shape,np.nan)
+    thl_par_j_precalc[:, 1:-1] = thlm[:, 1:-1] - thlm[:, 0:-2] * exp_mu_dzm[:, 1:-2] \
+                             - ( thlm[:, 1:-1] - thlm[:, 0:-2] ) * entrain_coef[:, 1:-2]
 
-    rt_par_j_precalc = rtm[:, 1:] - rtm[:, :-1] * exp_mu_dzm[:, 1:] - \
-                       (rtm[:, 1:] - rtm[:, :-1]) * entrain_coef[:, 1:]
+    
+    rt_par_j_precalc = np.full(rtm.shape,np.nan) 
+    rt_par_j_precalc[:, 1:-1] = rtm[:, 1:-1] - rtm[:, 0:-2] * exp_mu_dzm[:, 1:-2] \
+                            - ( rtm[:, 1:-1] - rtm[:, 0:-2]) * entrain_coef[:, 1:-2]
+    
+    thl_par_1 = np.full(thlm.shape,np.nan) 
+    thl_par_1[:, 1:] = thlm[:, 1:] - (thlm[:, 1:] - thlm[:, :-1]) * entrain_coef[:, 1:-1]
 
+    tl_par_1 = thl_par_1*exner
+    
+    rt_par_1 = np.full(rtm.shape,np.nan) 
+    rt_par_1[:, 1:] = rtm[:, 1:] - (rtm[:, 1:] - rtm[:, :-1]) * entrain_coef[:, 1:-1]
+
+    rsatl_par_1 = sat_mixrat_liq(tl_par_1, p_in_Pa)
+    
+    tl_par_1_sqd = tl_par_1**2
+    
+    s_par_1 = (rt_par_1 - rsatl_par_1) * tl_par_1_sqd / ( tl_par_1_sqd + Lv2_coef * rsatl_par_1 )
+
+    rc_par_1 = np.maximum( s_par_1, 0.0 )
+
+    thv_par_1 = thl_par_1 + ep1 * thv_ds * rt_par_1 + Lv_coef * rc_par_1
+
+    dCAPE_dz_1 = grav_on_thvm * ( thv_par_1 - thvm )
+
+    CAPE_incr_1 = 0.5 * dCAPE_dz_1 * dzm[:,1:]
+        
     for i in range(ngrdcol):
         Lscale_up_max_alt = 0.0  # Initial max height for upward mixing
         for k in range(nzt - 2):
-            if tke_i[i, k] > 0:
-                tke = tke_i[i, k]
+            if tke_i[i, k] + CAPE_incr_1[i, k + 1] > 0.0:
+                tke = tke_i[i, k] + CAPE_incr_1[i, k + 1]
                 j = k + 2
-                thl_par_j = thl_par_j_precalc[i, k + 1]
-                rt_par_j = rt_par_j_precalc[i, k + 1]
-                dCAPE_dz_j_minus_1 = grav_on_thvm[i, k + 1] * (thl_par_j - thvm[i, k + 1])
+                
+                thl_par_j = thl_par_1[i, k + 1]
+                rt_par_j = rt_par_1[i, k + 1]
+                dCAPE_dz_j_minus_1 = dCAPE_dz_1[i, k+1]
+                
+                while j < nzt-1:
 
-                while j < nzt:
                     thl_par_j = thl_par_j_precalc[i, j] + thl_par_j * exp_mu_dzm[i, j]
+                    
                     rt_par_j = rt_par_j_precalc[i, j] + rt_par_j * exp_mu_dzm[i, j]
 
-                    thv_par_j = thl_par_j + ep1 * thv_ds[i, j] * rt_par_j
+                    tl_par_j = thl_par_j*exner[i,j]
+
+                    rsatl_par_j = sat_mixrat_liq(tl_par_j,  p_in_Pa[i,j])
+
+                    tl_par_j_sqd = tl_par_j**2
+
+                    s_par_j = ( rt_par_j - rsatl_par_j ) * tl_par_j_sqd \
+                              / ( tl_par_j_sqd + Lv2_coef * rsatl_par_j )
+
+                    rc_par_j = np.maximum( s_par_j, 0.0 )
+
+                    thv_par_j = thl_par_j + ep1 * thv_ds[i, j] * rt_par_j + Lv_coef[i,j] * rc_par_j
+                    
                     dCAPE_dz_j = grav_on_thvm[i, j] * (thv_par_j - thvm[i, j])
 
                     CAPE_incr = 0.5 * (dCAPE_dz_j + dCAPE_dz_j_minus_1) * dzm[i, j]
-
+                    
                     if tke + CAPE_incr <= 0:
                         break
 
                     dCAPE_dz_j_minus_1 = dCAPE_dz_j
+                    
                     tke += CAPE_incr
+                    
                     j += 1
 
-                Lscale_up[i, k] += zt[j - 1] - zt[k]
+                Lscale_up[i, k] += zt[i, j - 1] - zt[i, k]
 
-                if j < nzt:
-                    Lscale_up[i, k] += (-tke / dCAPE_dz_j) if dCAPE_dz_j != 0 else 0
+                if j < nzt-1:
+                    if ( np.abs( dCAPE_dz_j - dCAPE_dz_j_minus_1 ) * 2 <= \
+                         np.abs( dCAPE_dz_j + dCAPE_dz_j_minus_1 ) * eps ):
+                        Lscale_up[i, k] += (-tke / dCAPE_dz_j)
+                        # print('if 1')
+                        # print(Lscale_up[i])
+                    else:
+                        invrs_dCAPE_diff = 1.0 / ( dCAPE_dz_j - dCAPE_dz_j_minus_1 )
+                        
+                        Lscale_up[i,k] += -dCAPE_dz_j_minus_1 * invrs_dCAPE_diff * dzm[i,j] \
+                                         - np.sqrt( dCAPE_dz_j_minus_1**2 - 2.0 * tke * invrs_dzm[i,j] \
+                                            * ( dCAPE_dz_j - dCAPE_dz_j_minus_1 ) ) \
+                                         * invrs_dCAPE_diff  * dzm[i,j]
+                        # print('if 2')
+                        # print(Lscale_up[i])
 
-                if zt[k] + Lscale_up[i, k] < Lscale_up_max_alt:
-                    Lscale_up[i, k] = Lscale_up_max_alt - zt[k]
-                else:
-                    Lscale_up_max_alt = zt[k] + Lscale_up[i, k]
+            else:
+                Lscale_up[i,k] += - np.sqrt( -2.0 * tke_i[i,k] * dzm[i,k+1] * dCAPE_dz_1[i,k+1]  ) \
+                                    / dCAPE_dz_1[i,k+1]
+                    # print('if 3')
+                    # print(Lscale_up[i])
+                
+            if zt[i, k] + Lscale_up[i, k] < Lscale_up_max_alt:
+                Lscale_up[i, k] = Lscale_up_max_alt - zt[i, k]
+            else:
+                Lscale_up_max_alt = Lscale_up[i, k] + zt[i, k]
 
-    # Downward Mixing Length Calculation
-    Lscale_down_min_alt = zt[-1]
+    
+    ## Downward Mixing Length Calculation
 
+    thl_par_j_precalc[:, :-1] = thlm[:, :-1] - thlm[:, 1:] * exp_mu_dzm[:, 1:-1] \
+                            - ( thlm[:, :-1] - thlm[:, 1:]) * entrain_coef[:, 1:-1]
+    
+    rt_par_j_precalc[:, :-1] = rtm[:, :-1] - rtm[:, 1:] * exp_mu_dzm[:, 1:-1] \
+                            - (rtm[:, :-1] - rtm[:, 1:]) * entrain_coef[:, 1:-1]
+
+    
+    thl_par_1[:, :-1] = thlm[:, :-1] - (thlm[:, :-1] - thlm[:, 1:]) * entrain_coef[:, 1:-1]
+
+    tl_par_1 = thl_par_1*exner
+    
+    rt_par_1[:, :-1] = rtm[:, :-1] - (rtm[:, :-1] - rtm[:, 1:] ) * entrain_coef[:, 1:-1]
+
+    rsatl_par_1 = sat_mixrat_liq(tl_par_1, p_in_Pa)
+
+    tl_par_1_sqd = tl_par_1**2
+
+    s_par_1 = (rt_par_1 - rsatl_par_1) * tl_par_1_sqd / ( tl_par_1_sqd + Lv2_coef * rsatl_par_1 )
+
+    rc_par_1 = np.maximum( s_par_1, 0.0 )
+
+    thv_par_1 = thl_par_1 + ep1 * thv_ds * rt_par_1 + Lv_coef * rc_par_1
+
+    dCAPE_dz_1 = grav_on_thvm * ( thv_par_1 - thvm )
+
+    CAPE_incr_1 = 0.5 * dCAPE_dz_1 * dzm[:,1:]
+    
     for i in range(ngrdcol):
-        for k in range(nzt - 2, 1, -1):
-            if tke_i[i, k] > 0:
-                tke = tke_i[i, k]
+        Lscale_down_min_alt = zt[i, -1]
+        for k in range(nzt - 1, 1, -1):
+            if tke_i[i, k] - CAPE_incr_1[i, k - 1] > 0.0:
+                tke = tke_i[i, k] - CAPE_incr_1[i, k - 1]
                 j = k - 2
-                thl_par_j = thl_par_j_precalc[i, k - 1]
-                rt_par_j = rt_par_j_precalc[i, k - 1]
-                dCAPE_dz_j_plus_1 = grav_on_thvm[i, k - 1] * (thl_par_j - thvm[i, k - 1])
+    
+                thl_par_j = thl_par_1[i, k - 1]
+                rt_par_j = rt_par_1[i, k - 1]
+                dCAPE_dz_j_plus_1 = dCAPE_dz_1[i, k-1]
 
                 while j >= 0:
-                    thl_par_j = thl_par_j_precalc[i, j] + thl_par_j * exp_mu_dzm[i, j + 1]
-                    rt_par_j = rt_par_j_precalc[i, j] + rt_par_j * exp_mu_dzm[i, j + 1]
+  
+                    thl_par_j = thl_par_j_precalc[i, j] + thl_par_j * exp_mu_dzm[i, j+1]
+                    
+                    rt_par_j = rt_par_j_precalc[i, j] + rt_par_j * exp_mu_dzm[i, j+1]
 
-                    thv_par_j = thl_par_j + ep1 * thv_ds[i, j] * rt_par_j
+                    tl_par_j = thl_par_j*exner[i,j]
+
+                    rsatl_par_j = sat_mixrat_liq(tl_par_j,  p_in_Pa[i,j])
+
+                    tl_par_j_sqd = tl_par_j**2
+
+                    s_par_j = ( rt_par_j - rsatl_par_j ) * tl_par_j_sqd \
+                              / ( tl_par_j_sqd + Lv2_coef * rsatl_par_j )
+
+                    rc_par_j = np.maximum( s_par_j, 0.0 )
+
+                    thv_par_j = thl_par_j + ep1 * thv_ds[i, j] * rt_par_j + Lv_coef[i,j] * rc_par_j
+                    
                     dCAPE_dz_j = grav_on_thvm[i, j] * (thv_par_j - thvm[i, j])
 
-                    CAPE_incr = 0.5 * (dCAPE_dz_j + dCAPE_dz_j_plus_1) * dzm[j + 1]
+                    CAPE_incr = 0.5 * (dCAPE_dz_j + dCAPE_dz_j_plus_1) * dzm[i, j+1]
 
                     if tke - CAPE_incr <= 0:
                         break
 
                     dCAPE_dz_j_plus_1 = dCAPE_dz_j
+                    
                     tke -= CAPE_incr
+                    
                     j -= 1
 
-                Lscale_down[i, k] += zt[k] - zt[j + 1]
+                Lscale_down[i, k] += zt[i, k] - zt[i,j + 1]
 
                 if j >= 0:
-                    Lscale_down[i, k] += (tke / dCAPE_dz_j) if dCAPE_dz_j != 0 else 0
+                    if (np.abs( dCAPE_dz_j - dCAPE_dz_j_plus_1 ) * 2 <= \
+                        np.abs( dCAPE_dz_j + dCAPE_dz_j_plus_1 ) * eps ):
+                        Lscale_down[i, k] += tke / dCAPE_dz_j
+                    else:
+                        invrs_dCAPE_diff = 1.0 / ( dCAPE_dz_j - dCAPE_dz_j_plus_1 )
+                        
+                        Lscale_down[i,k] += -dCAPE_dz_j_plus_1 * invrs_dCAPE_diff * dzm[i,j+1] \
+                                         + np.sqrt( dCAPE_dz_j_minus_1**2 + 2.0 * tke * invrs_dzm[i,j+1] \
+                                            * ( dCAPE_dz_j - dCAPE_dz_j_plus_1 ) ) \
+                                         * invrs_dCAPE_diff  * dzm[i,j+1]                    
 
-                if zt[k] - Lscale_down[i, k] > Lscale_down_min_alt:
-                    Lscale_down[i, k] = zt[k] - Lscale_down_min_alt
-                else:
-                    Lscale_down_min_alt = zt[k] - Lscale_down[i, k]
+            else:
+                Lscale_down[i,k] +=  np.sqrt( 2.0 * tke_i[i,k] * dzm[i,k] * dCAPE_dz_1[i,k-1]  ) \
+                                    / dCAPE_dz_1[i,k-1]
+                
+            if zt[i,k] - Lscale_down[i,k] > Lscale_down_min_alt:
+                Lscale_down[i,k] = zt[i,k] - Lscale_down_min_alt
+            else:
+                Lscale_down_min_alt = zt[i,k] - Lscale_down[i,k]
+        
+    ## Final Mixing Length Calculation
+    
+    if l_implemented:
+        lminh = np.maximum(0.0, Lscale_sfclyr_depth - ( zt - zm[:,0:1] ) ) \
+                  * lmin * invrs_Lscale_sfclyr_depth
 
-    # Final Mixing Length Calculation
-    for i in range(ngrdcol):
-        for k in range(nzt):
-            lminh = max(0, Lscale_sfclyr_depth - zt[k]) * lmin / Lscale_sfclyr_depth
-            Lscale_up[i, k] = max(lminh, Lscale_up[i, k])
-            Lscale_down[i, k] = max(lminh, Lscale_down[i, k])
-            Lscale[i, k] = np.sqrt(Lscale_up[i, k] * Lscale_down[i, k])
+    Lscale_up = np.maximum( lminh, Lscale_up)
+    Lscale_down = np.maximum( lminh, Lscale_down)
+    
+    Lscale = np.sqrt( Lscale_up * Lscale_down )
 
-        Lscale[i, -1] = Lscale[i, -2]
-        Lscale[i, :] = np.minimum(Lscale[i, :], Lscale_max[i])
+    # Lscale[:, -1] = Lscale[:, -2]
+    Lscale = np.minimum(Lscale, Lscale_max)
 
     return Lscale, Lscale_up, Lscale_down
+
+def sat_mixrat_liq(T, p):
+    """
+    Calculate the saturation mixing ratio given temperature (K) and pressure (Pa).
+    Using the Clausius-Clapeyron equation approximation.
+    """
+    es = 6.112 * np.exp((17.67 * (T - 273.15)) / (T - 29.65)) * 100  # Convert hPa to Pa
+    epsilon = 0.622  # Ratio of molecular weights (water vapor/dry air)
+    return epsilon * es / (p - es)
+
+def dqsat_dT(T, p):
+    """
+    Derivative of saturation mixing ratio with respect to temperature.
+    """
+    es = 6.112 * np.exp((17.67 * (T - 273.15)) / (T - 29.65)) * 100  # Convert hPa to Pa
+    des_dT = es * (17.67 * 243.5) / ((T - 29.65)**2)  # Derivative of es w.r.t T
+    epsilon = 0.622
+    return epsilon * des_dT / (p - es) - epsilon * es * des_dT / (p - es)**2
