9863841387de1393707ca69153b0ace940bf6912
Alex Connolly <adac@berkeley.edu>
2025-07-17 08:15:21 -0600

    BOMEX and DYCOMS trained as new clipped_ANN class current version has min = 0 and max=2 as in the CLUBB tunable param table

--- subgrid_parameterization/arch/ann.py
+++ subgrid_parameterization/arch/ann.py
@@ -59,3 +59,45 @@ class ANN(torch.nn.Module):
 
     def forward(self,x):
         return self.ops(x)
+
+class clipped_ANN(torch.nn.Module):
+    def __init__(self, N, range=[0,2], activation=torch.nn.ReLU):
+        '''
+        Packs sequence of artificial neural network layers in a list.
+        '''
+        super().__init__()
+        ops = []
+        # Following recipe for itertools.pairwise
+        NA, NB = itertools.tee(N)
+        next(NB, None)
+        n_iter = zip(NA, NB)
+        # Create all but the last linear layer and activation
+        for n_a, n_b in itertools.islice(n_iter, len(N) - 2):
+            ops.extend(
+                make_layer(
+                    in_features=n_a,
+                    out_features=n_b,
+                    activation=activation,
+                    batch_norm=False,
+                    bias=False,
+                )
+            )
+        # Final layer: no activation
+        n_a, n_b = next(n_iter)
+        ops.extend(
+            make_layer(
+                in_features=n_a,
+                out_features=n_b,
+                activation=None,
+                batch_norm=False,
+                bias=False,
+            )
+        )
+        # Bundle into Sequential for forward pass
+        self.ops = torch.nn.Sequential(*ops)
+
+        self.min = range[0]
+        self.max = range[1]
+
+    def forward(self,x):
+        return self.ops(x).clamp(min = self.min, max = self.max) 
