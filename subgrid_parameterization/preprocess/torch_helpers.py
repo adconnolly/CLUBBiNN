833a445eaeb832dee455ecda0b8145a901b89ab4
Alex Connolly <adac@berkeley.edu>
2025-09-11 12:11:41 -0600

    split_dataset can use less than all data, supports giving train_ratio only or with test_ratio

--- subgrid_parameterization/preprocess/torch_helpers.py
+++ subgrid_parameterization/preprocess/torch_helpers.py
@@ -2,15 +2,28 @@ import numpy as np
 import torch.utils.data as Data  # The ready-to-use class for dumbies, e.g. Alex Connolly, to write this code
 
 
-def split_dataset(dataset, ratio):
+def split_dataset(dataset, ratios):
+    try:
+        test_ratio = 1 - ratios
+        train_ratio = ratios
+    except:
+        if len(ratios) == 1:
+            train_ratio = ratios[0]
+            test_ratio = 1 - train_ratio
+        elif len(ratios) == 2:
+            train_ratio, test_ratio = ratios
+
+    assert train_ratio + test_ratio <= 1.0
+
     x, y = dataset.tensors
 
     n = len(x)
-    train_n = int(n * ratio)
+    train_n = int(n * train_ratio)
+    test_n = int(n * test_ratio)
 
     rand_indices = np.random.permutation(np.arange(n))
     train_indices = rand_indices[:train_n]
-    test_indices = rand_indices[train_n:]
+    test_indices = rand_indices[-test_n:]
 
     x_train = x[train_indices]
     x_test = x[test_indices]
