b24f38eb2e64a599f6b3621ea9722aba297688cd
Jack Atkinson <jwa34@cam.ac.uk>
2025-10-20 09:29:12 +0100

    Refactor EarlyStopper
        * make directly importable in code via namespace in __init__.py
        * clean other elements of the EarlyStopper code
        * reflect these changes in the training notebooks

---
+++ src/subgrid_parameterization/util/__init__.py
@@ -0,0 +1,5 @@
+"""Module providing useful utilities for training."""
+
+from .earlystopper import EarlyStopper
+
+__all__ = ["EarlyStopper"]
--- src/subgrid_parameterization/util/earlystopper.py
+++ src/subgrid_parameterization/util/earlystopper.py
@@ -1,8 +1,12 @@
-import torch
+"""Module defining an early stopper for the training process."""
+
 import numpy as np
+import torch
 
 
 class EarlyStopper:
+    """Class to trigger early stopping if loss threshold is reached in training."""
+
     def __init__(self, patience=1, min_delta=0):
         self.patience = patience
         self.min_delta = min_delta
@@ -10,6 +14,12 @@ class EarlyStopper:
         self.min_validation_loss = np.inf
 
     def early_stop(self, val_loss):
+        """
+        Check whether to trigger early stopping.
+
+        Based on loss not decreasing below best yet for a certain number of steps set
+        by the patience attribute.
+        """
         if val_loss < self.min_validation_loss:
             self.min_validation_loss = val_loss
             self.counter = 0
